3fbfced930d7980e01ecb01da67a8dcfa233e9fc
JuandiGuti <juandiegoguti2302@gmail.com>
2025-10-14 11:28:58 -0600

    fix(frontend): -

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,24 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
+    <div style={{ padding: "20px 100px" }}>
       <h1>Disagro FakeShop</h1>
+      <div style={{ padding: "8px" }} />
       <p>
-        Ir a <a href="/products">Productos</a>
+        Ir hacia
+        <Link
+          href="/products"
+          style={{
+            border: "1px solid #ddd",
+            padding: "4px 8px",
+            borderRadius: 6,
+            background: "#ddd",
+            color: "#000000ff",
+          }}
+        >
+          Productos
+        </Link>
       </p>
     </div>
   );
